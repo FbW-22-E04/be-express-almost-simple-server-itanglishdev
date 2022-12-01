97bb3cc36c45441a84c25a328a73bf18b42ae3e4
itanglishdev <chrishethertonweb@gmail.com>
2022-12-01 10:22:10 +0100

    set up

--- server.js
+++ server.js
@@ -20,6 +20,29 @@ const sendTime = (req, res) => {
 
 const getRandom = (req, res) => res.send(Math.ceil(Math.random() * 100).toString())
 
+const checkNumber = (req, res) => {
+    console.log('value is ', req.params)
+
+    if (Number.isNaN(parseInt(req.params.value))) {
+        res.send('Cannot convert in number')
+    } else {
+        res.send('Can be converted to number')
+    }
+
+}
+const checkNumberQuery = (req, res) => {
+    console.log('value is ', req.query)
+
+    if (Number.isNaN(parseInt(req.query.value))) {
+        res.send('Cannot convert in number')
+    } else {
+        res.send('Can be converted to number')
+    }
+
+}
+
 app.get('/hello', greetings)
 app.get('/time', sendTime)
 app.get('/random', getRandom)
+app.get('/isnumber/:value', checkNumber)
+app.get('/isnumberquery', checkNumberQuery)
